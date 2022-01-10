3deb30abed300285587e914996ff593bada47a30
Evan-6 <61612621+Evan-6@users.noreply.github.com>
2022-01-10 18:49:42 +0800

    Update multiArray.c

--- multiArray.c
+++ multiArray.c
@@ -18,3 +18,30 @@ int main(){
 	}
 	printf("%d",add(a,m,n));
 } 
+
+
+
+#include<cstdio>
+#include<cstdlib>
+int add(int **a,int m,int n){
+	int sum=0;
+	for(int i=0;i<m;i++){
+		for(int j=0;j<n;j++){
+			sum+=a[i][j];
+		}
+	}
+	return sum;
+}
+int main(){
+	int m,n;
+	scanf("%d%d",&m,&n);
+	int **a=(int **)malloc(sizeof(int *)* m);
+	for(int i=0;i<m;i++)a[i]=(int *)malloc(sizeof(int)* n);
+
+	for(int i=0;i<m;i++){
+		for(int j=0;j<n;j++){
+            scanf("%d",&a[i][j]);
+        }
+	}
+	printf("%d",add(a,m,n));
+} 
